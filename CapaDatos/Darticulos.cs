387aca3ff5d8ece79d3122b8c70bb46d3f16d284
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-05-22 11:54:02 -0600

    Agregar archivos de proyecto.

---
+++ CapaDatos/Darticulos.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//dll de la conexion
+using conexionesDB;
+
+namespace CapaDatos
+{
+    public class Darticulos
+    {
+        public static string Servidor {  get; set; }
+        public static string user { get; set; }
+        public static string password { get; set; }
+        public static string database { get; set; }
+       
+        #region constructor
+        public Darticulos() { }
+        public Darticulos(int ProductID,string NombreProducto,string Descripcion,int precio)
+        {
+            ProductoID = ProductID;
+            NombreProductos = NombreProducto;
+            Descripciones = Descripcion;
+            precios = precio;
+            
+            conexionDB conexionDB = new conexionDB();
+            conexionDB.ServidorNo = Servidor;
+            conexionDB.Users = user;
+            conexionDB.Password = password; 
+            conexionDB.DataBase = database;
+        }
+        #endregion
+
+        #region propiedades de la clase
+        public int ProductoID { get; set; } 
+        public string NombreProductos { get; set; }
+        public string Descripciones { get; set; }
+        public int precios { get; set; }
+        #endregion
+
+        #region insertarproductos
+        public string insertarproductos(Darticulos darticulos)
+        {
+            conexionDB conexionDB = new conexionDB();
+            string respuesta = "";
+            var conexionsql = new SqlConnection(conexionDB.ConexionStatic(Servidor,user,password,database));
+            try
+            {
+                conexionsql.Open(); //abrimos la conexion del sql server
+
+                var comandosql = new SqlCommand("[sp_InsertarProducto]");
+                comandosql.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+            }catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+
+            }
+            return respuesta;
+        }
+        #endregion
+    }
+}
---
+++ conexionesDB/DBconexion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conexionesDB
+{
+    public class conexionDB
+    {
+        #region Variables_Globales
+        public static String ServidorNo { get; set; }   
+        public static String Users { get; set;}
+        public static String Password {  get; set; }
+        public static String DataBase { get; set; }
+        #endregion
+
+
+        #region constructor
+        public conexionDB()
+        {
+            ServidorNo = string.Empty;
+            Users = string.Empty;
+            Password = string.Empty;
+            DataBase = string.Empty;
+        }
+        #endregion
+
+        #region conexionstatica
+        public static String ConexionStatic(string Servidor,string User,string Passwordd,string Base_Datos)
+        {
+            #region Variablesconcadenaconexion
+            ServidorNo = "ALEXIS";
+            Users = "alexis";
+            Password = "12345";
+            DataBase = "InventoryManagement";
+            #endregion
+
+            ServidorNo = Servidor;
+            Users = User;
+            Password = Passwordd;
+            DataBase = Base_Datos;
+            return $"Data Source={ServidorNo};Initial Catalog={DataBase};User ID={Users};Password={Password};TrustServerCertificate=true;";
+        }
+        #endregion
+    }
+}
