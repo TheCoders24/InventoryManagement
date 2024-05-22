a75633dcea722ef9dc8a2924919b40e73164337e
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-05-22 13:09:29 -0600

    add

---
+++ CapaDatos/DUsuarios.cs
@@ -0,0 +1,88 @@
+﻿using conexionesDB;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class DUsuarios
+    {
+        /*
+     UserID INT PRIMARY KEY,
+     Username VARCHAR(50) NOT NULL,
+     Contraseña VARCHAR(50) NOT NULL,
+     RoleID INT NOT NULL,
+     FOREIGN KEY (RoleID) REFERENCES Roles(RoleID)
+        */
+
+        #region variables_globales_conexiondb
+        public static string Servidor { get; set; }
+        public static string user { get; set; }
+        public static string password { get; set; }
+        public static string database { get; set; }
+        #endregion
+
+        #region propiedades
+        public int UserID { get; set; }
+        public  string UserName { get; set; }
+        public  string Contraseña { get; set; }
+        public  int RoleID { get; set; }
+        #endregion
+
+        #region constructor
+        public DUsuarios() { }
+        public DUsuarios(int userid,string username,string contrasena,int roleid) { 
+            
+            UserID = userid;
+            UserName = username;
+            Contraseña = contrasena;
+            RoleID = roleid;
+
+            conexionDB conexionDB = new conexionDB();
+            conexionDB.ServidorNo = Servidor;
+            conexionDB.Users = user;
+            conexionDB.Password = password;
+            conexionDB.DataBase = database;
+        }
+        #endregion
+
+        #region insertar_usuario
+        public string insertarusuario(DUsuarios dUsuarios)
+        {
+
+            string respuesta = "";
+            var conexionsql = new SqlConnection(conexionDB.ConexionStatic(Servidor,user,password,database));
+
+            try
+            {
+
+                conexionsql.Open();
+
+                var comandosql = new SqlCommand("[sp_InsertarUsuario]",conexionsql);
+                comandosql.CommandType = CommandType.StoredProcedure;
+
+                var userid = new SqlParameter("@UserID",SqlDbType.Int);
+                userid.Direction = ParameterDirection.Output;
+                comandosql.Parameters.Add(userid);
+
+            }catch (Exception ex)
+            {
+                respuesta = ex.Message;
+            }
+            finally
+            {
+                if(conexionsql.State == ConnectionState.Open)
+                    conexionsql.Close();
+            }
+            return respuesta;
+
+        }
+        #endregion
+
+    }
+}
--- CapaDatos/Darticulos.cs
+++ CapaDatos/Darticulos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -11,11 +12,13 @@ namespace CapaDatos
 {
     public class Darticulos
     {
+        #region variables_globales_conexiondb
         public static string Servidor {  get; set; }
         public static string user { get; set; }
         public static string password { get; set; }
         public static string database { get; set; }
-       
+        #endregion
+
         #region constructor
         public Darticulos() { }
         public Darticulos(int ProductID,string NombreProducto,string Descripcion,int precio)
@@ -50,20 +53,177 @@ namespace CapaDatos
             {
                 conexionsql.Open(); //abrimos la conexion del sql server
 
-                var comandosql = new SqlCommand("[sp_InsertarProducto]");
+                //Establecemos el comando sql 
+                var comandosql = new SqlCommand("[sp_InsertarProducto]",conexionsql);
                 comandosql.CommandType = System.Data.CommandType.StoredProcedure;
 
+                //parametros para el comando de sql sel pocedimiento almacenado
+                var productID = new SqlParameter("@ProductID", SqlDbType.Int);
+                productID.Direction = ParameterDirection.Output;
+                comandosql.Parameters.Add(productID);
 
-            }catch (Exception ex)
+
+                var Nombre_producto = new SqlParameter("@NombreProducto", SqlDbType.VarChar, 50);
+                Nombre_producto.Value = darticulos.NombreProductos;
+                comandosql.Parameters.Add(Nombre_producto);
+
+                var Descripcion = new SqlParameter("@Descripcion", SqlDbType.VarChar, 200);
+                Descripcion.Value = darticulos.Descripciones;
+                comandosql.Parameters.Add(Descripcion);
+
+                var precio = new SqlParameter("@Precio",SqlDbType.Int);
+                precio.Direction = ParameterDirection.Output;
+                comandosql.Parameters.Add(precio);
+
+                respuesta = comandosql.ExecuteNonQuery() ==1 ? "Ok" :"no se puede insertar el registro";
+
+            }
+            catch (Exception ex)
             {
-
+                respuesta = ex.Message;
             }
             finally
             {
-
+                if (conexionsql.State == ConnectionState.Open)
+                {
+                    conexionsql.Close();
+                }
             }
             return respuesta;
         }
         #endregion
+
+        #region actualizar_productos
+        public string ActualizarProductos(Darticulos darticulos)
+        {
+            string respuesta = "";
+            conexionDB conexionDB = new conexionDB();
+            var conexionsql = new SqlConnection(conexionDB.ConexionStatic(Servidor, user, password, database));
+
+            try
+            {
+
+                conexionsql.Open();
+                var comandosql = new SqlCommand("[sp_ActualizarProducto]", conexionsql);
+                comandosql.CommandType = CommandType.StoredProcedure;
+
+                //parametros para el comando de sql sel pocedimiento almacenado
+                var productID = new SqlParameter("@ProductID", SqlDbType.Int);
+                productID.Direction = ParameterDirection.Output;
+                comandosql.Parameters.Add(productID);
+
+                var Nombre_producto = new SqlParameter("@NombreProducto", SqlDbType.VarChar, 50);
+                Nombre_producto.Value = darticulos.NombreProductos;
+                comandosql.Parameters.Add(Nombre_producto);
+
+                var Descripcion = new SqlParameter("@Descripcion", SqlDbType.VarChar, 200);
+                Descripcion.Value = darticulos.Descripciones;
+                comandosql.Parameters.Add(Descripcion);
+
+                var precio = new SqlParameter("@Precio", SqlDbType.Int);
+                precio.Direction = ParameterDirection.Output;
+                comandosql.Parameters.Add(precio);
+
+                respuesta = comandosql.ExecuteNonQuery() == 1 ? "Ok" : "no se puede insertar el registro";
+
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message;
+            }
+            finally
+            {
+                if (conexionsql.State == ConnectionState.Open)
+                    conexionsql.Close();
+            }
+            return respuesta;   
+        }
+        #endregion
+
+        #region EliminarProductos
+        public string EliminarProductos(Darticulos darticulos)
+        {
+            string respuesta = "";
+            conexionDB conexionDB = new conexionDB();
+            var conexionsql = new SqlConnection(conexionDB.ConexionStatic(Servidor,user,password,database));
+            try
+            {
+
+                conexionsql.Open();
+                //establecemos el comando sql para el procedimientos almacenados
+                var comandosql = new SqlCommand("[sp_EliminarProducto]", conexionsql);
+                comandosql.CommandType = CommandType.StoredProcedure;
+
+                //paramentros para el command sql del procedimientos almacenados
+                var productid = new SqlParameter("@ProductID", SqlDbType.Int);
+                productid.Value = darticulos.ProductoID;
+                comandosql.Parameters.Add(productid);
+
+                //ejecutamos del comando
+                respuesta = comandosql.ExecuteNonQuery() == 1 ? "ok" : "no se puede eliminar el registro";
+
+            }
+            catch (Exception ex)
+            {
+                respuesta = ex.Message;
+            }finally
+            {
+                if(conexionsql.State == ConnectionState.Open)
+                    conexionsql.Close();
+            }
+            return respuesta;
+        }
+        #endregion
+
+        #region mostrar_productos
+        public DataTable mostrar_productos()
+        {
+            conexionDB conexionDB = new conexionDB();
+            var resultadotablas = new DataTable("Productos");
+            var conexionsql = new SqlConnection(conexionDB.ConexionStatic(Servidor,user,password,database));
+            try
+            {
+                var comandosql = new SqlCommand("[sp_MostrarProductos]", conexionsql);
+                comandosql.CommandType = CommandType.StoredProcedure;    
+
+                var sqlData = new SqlDataAdapter(comandosql);
+                sqlData.Fill(resultadotablas);
+
+            }catch(Exception)
+            {
+                resultadotablas = null;
+            }
+            return resultadotablas;
+        }
+        #endregion
+
+        #region MetodoStockArticulos
+        //Metodo Mostrar
+        public DataTable StockArticulos()
+        {
+            //Cadena de conexion y DataTable (tabla)
+            var resultadoTabla = new DataTable("Productos");
+            var conexionSql = new SqlConnection(conexionDB.ConexionStatic(Servidor,user,password,database));
+            try
+            {
+                var comandoSql = new SqlCommand("[sp_MostrarStockProductos]", conexionSql);
+                comandoSql.CommandType = CommandType.StoredProcedure;
+
+                var SqlDat = new SqlDataAdapter(comandoSql);
+                SqlDat.Fill(resultadoTabla);
+
+            }
+            catch (Exception)
+            {
+                resultadoTabla = null;
+            }
+
+            return resultadoTabla;
+
+
+        }
+        #endregion
+
     }
 }
