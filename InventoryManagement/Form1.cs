1befaf2b89b03ee27106c2377e0b053ac90a3694
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2024-05-23 20:31:09 -0600

    add login ok

--- InventoryManagement/Form1.cs
+++ InventoryManagement/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CapaNegocio;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,17 @@ namespace InventoryManagement
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = textBox1.Text;
+            string password = textBox2.Text;
+
+            DataTable datos = NUsuarios.login(usuario.ToString(),password.ToString());
+            if (datos == null)
+                MessageBox.Show("usuarios" + usuario.ToString() + "password" + password.ToString());
+                
         }
     }
 }
